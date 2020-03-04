302636ebb34a96e056fbd84eb5c3336f81e40221
ekiprop <evanskiprop1@gmail.com>
2020-03-04 21:51:20 +0300

    data model retrofit

---
+++ app/src/main/java/com/ekiprop/smartswitch/DataModel.kt
@@ -0,0 +1,25 @@
+package com.ekiprop.smartswitch
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+class DataModel (
+
+    @Expose
+    @SerializedName("id")
+    val id: Integer,
+    @Expose
+    @SerializedName("switch0")
+    val switch0: String,
+    @Expose
+    @SerializedName("switch1")
+    val switch1: String,
+    @Expose
+    @SerializedName("swtch2")
+    val switch2: String,
+    @SerializedName("switch3")
+    val switch3: String,
+    @SerializedName("phone_no")
+    val phone_no: String
+
+    )
